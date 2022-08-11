65ca1a393033fdcffcef73743c8d38e2afe8b679
dalto135 <67397294+dalto135@users.noreply.github.com>
2022-08-11 18:03:23 -0400

    can be used with strings and arrays

--- permutation.js
+++ permutation.js
@@ -1,20 +1,21 @@
-function permPerm(str, prefix) {
-    if (str.length == 0) {
+function permPerm(input, prefix) {
+    if (input.length == 0) {
         console.log(prefix);
     }
     else {
-        for (let i = 0; i < str.length; ++i) {
-            //rem is the full str minus the character at i
-            let rem = str.substring(0, i) + str.substring(i + 1);
-            permPerm(rem, prefix + str[i]);
+        for (let i = 0; i < input.length; ++i) {
+            //rem is the full input minus the element at i
+            let rem = input.slice(0, i).concat(input.slice(i + 1));
+            permPerm(rem, prefix.concat(input[i]));
         }
     }
 }
 
-function perm(str) {
-    permPerm(str, "");
+function perm(input) {
+    permPerm(input, "");
 }
 
 // Runtime: N!(N^2)
 let str = "cat";
-perm(str);
+let array = [1, 2, 3];
+perm(array);
